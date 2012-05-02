e0bdc6b56856d1f1eebf34bb12666a2f7aa095af
yon <Lighthawky@gmail.com>
2012-05-02 16:52:57 +0300

    intitial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lunchtime::Application.config.secret_token = 'e4c876b3b8ad28962ba44c13314f4f6daaa3b8dab6a25b56e08348741a594a68475cf974e0a06f88329e5572a1173bdc991e75b0180226fb069e6d71beabda83'
