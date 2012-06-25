a99019d7981583a2d283f79004bbdaf9a16efd7f
yon <Lighthawky@gmail.com>
2012-06-25 23:57:22 +0300

    basic controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -12,4 +12,29 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.new
   end
 
+  def create
+    @restaurant = Restaurant.new(params[:restaurant])
+    if @restaurant.save
+      respond_to do |format|
+          format.html { redirect_to restaurants_path }
+          format.json { render :json => @restaurants }
+      end
+    else
+      respond_to do |format|
+          format.html { render :action => "new" }
+          format.json { render :json => {:errors => @restaurants.errors} }
+      end
+    end
+  end
+
+  ## PUT /babies/1
+  #def update
+  #  @baby = Baby.find(params[:id])
+  #  if @baby.update_attributes(params[:baby])
+  #    redirect_to(@baby, :notice => 'Baby was successfully updated.')
+  #  else
+  #    render :action => "edit"
+  #  end
+  #end
+
 end
